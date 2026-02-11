79cd040f3d7e14f2b539c7945b7a0f098707df12
Mohammed Tosif <techie.tosif@gmail.com>
2026-02-11 18:53:55 +0530

    Comment out old BMI print and add formatted output

--- main.py
+++ main.py
@@ -9,5 +9,7 @@ bmi = (weight/(height**2))
 
 # print(round(bmi)) #Print 31 use round fn for decimal conversion
 
-print(round(bmi,2)) #Print 30.85
+# print(round(bmi,2)) #Print 30.85
 
+# F-string for print with type conversion
+print(f"Your BMI is: {round(bmi,2)}kg")
